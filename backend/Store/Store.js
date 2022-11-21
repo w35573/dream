dd380e9b98d0fbe6dc3251584811b63aa9a73949
w35573 <prathamesh.redij@spit.ac.in>
2022-11-21 19:54:50 +0530

    added procfile

--- backend/Store/Store.js
+++ backend/Store/Store.js
@@ -4,4 +4,4 @@ const localStorage = new LocalStorage('./scratch');
 
 const user = localStorage.setItem('user', '12900788');
 
-const session_id = localStorage.setItem('session_id', 'a8c76aa9652dfc913622347e6879d68bd7a45991');
+const session_id = localStorage.setItem('session_id', '0b10b0a04cd1c9ac1b331ee625f208e1bcfcec90');
